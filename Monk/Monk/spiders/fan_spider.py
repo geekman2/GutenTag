207333495dacacc5feaae9cbf1e8cd57140f5651
geekman2 <geekmann2@gmail.com>
2016-07-13 22:30:53 -0700

    Implemented recursive parser, untested

--- Monk/Monk/spiders/fan_spider.py
+++ Monk/Monk/spiders/fan_spider.py
@@ -18,15 +18,21 @@ class FanfictionSpider(CrawlSpider):
 
     ]
 
-    def parse(self, response):
+    def parse_page(self, response):
         titles = response.xpath('//a[@class="stitle"]')
 
         for title in titles:
             item = MonkPageItem()
-            item['title'] = title.xpath('text()').extract()
-            item['url'] = sel.xpath('@href').extract()
+            title_text = title.xpath('text()').extract()
+            title_url = title.xpath('@href').extract()
+            item['title'] = title_text
+            item['url'] = title_url
+            request = scrapy.Request(url=title_url,
+                                     callback=self.parse_page2())
             yield item
 
-
+    def parse_page2(self, response):
+        item = MonkStoryItem
+        text = response.xpath('//*[@id="storytext"/text()]').extract()
 
 
