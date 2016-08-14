0f570cde9c5b725de77b3cd83dc428e056e7daa2
geekman2 <geekmann2@gmail.com>
2016-08-14 15:14:23 -0700

    Tokenization seems to be working, put into the worker because if we don't need sentences it's only one line

--- WordVectors/tokenizer.py
+++ WordVectors/tokenizer.py
@@ -15,28 +15,22 @@ import WordVectors.parser
 
 def tokenize(texts, parser=spacy.en.English()):
     # Intialize the parser and tokenize the text retrieved
-    nlp = English()
-    for doc in nlp.pipe(texts, n_threads=16):
+    for doc in parser.pipe(texts, n_threads=16):
         sentences = []
         for span in doc.sents:
             sent = [token.text for token in span]
             sentences.append(sent)
         yield sentences
 
-def chunks(cursor, n):
-    #Yield successive n-sized chunks from ls
-    for i in xrange(0, 100, n):
-        yield [x['text'] for x in cursor[i:i + n]]
 
 
-
-def worker(docChunk):
+def worker(doc):
     # Get the parsed data and update the document with tokenedText
-    parsed = tokenize(docChunk)
-    for sentence in parsed:
-        print(sentence)
-    # WordVectors.parser.docs.update_one({'_id': doc['_id']},
-    #                                   {'$set': {'tokenedText': parsed}, })
+    tokenizer = spacy.en.English()
+    parsedText = tokenizer(doc['text'])
+    parsedList = list((parsedText))
+    WordVectors.parser.docs.update_one({'_id': doc['_id']},
+                                       {'$set': {'tokenedText': parsedList}, })
 # DELETE THIS BRACE
 # '$unset':{'text':''}}) UNCOMMENTING WILL DELETE THE TEXT FIELD
 
@@ -46,7 +40,7 @@ def tokenizeMultiple():
     cur = WordVectors.parser.docs.find({'text': {'$exists': 'true'}},
                                        {'text': 1})
     pool = multiprocessing.Pool(8)
-    map(worker, chunks(cur[:100],10))
+    map(worker, cur[:1])
     pool.close()
     pool.join()
 
