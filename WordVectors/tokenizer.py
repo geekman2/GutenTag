5316f34195ad64602e694fb601176e51f9c0d2fa
parambharat <param.bharat@gmail.com>
2016-08-17 11:28:00 +0530

    Fixed tokenizer bug

--- WordVectors/tokenizer.py
+++ WordVectors/tokenizer.py
@@ -7,7 +7,8 @@
 # Copyright:    (c) Bharat Ramanathan
 # ------------------------------------------------------------------------------
 from __future__ import print_function, absolute_import
-import parser
+# import parser
+from var.mongoSim import simMongoDb
 from spacy.en import English
 from time import time
 from itertools import izip
@@ -20,22 +21,24 @@ def getText(cur):
 
 def tokenize(texts, parser=English()):
     for doc in parser.pipe(texts, n_threads=16):
-        yield [token.text for token in doc]
+        yield [[token.text for token in sent] for sent in doc.sents]
 
 
 def writeText(cur):
     texts, ids = izip(*getText(cur))
     for text, ids in izip(tokenize(texts), ids):
-        # print(text, ids) # - Uncomment for debug info.
+        # print(text, ids)  # - Uncomment for debug info.
         # parser.docs.update_one({'_id': doc['_id']},
         #                       {'$set': {'tokenedText': parsedList}})
         # DELETE THIS BRACE
         # '$unset':{'text':''}}) UNCOMMENTING WILL DELETE THE TEXT FIELD
+        pass
 
 
 if __name__ == '__main__':
-    data = parser.docs
-    cur = data.find({'text': {'$exists': 'true'}}, {'text': 1})
+    # data = parser.docs
+    # cur = data.find({'text': {'$exists': 'true'}}, {'text': 1})
+    data = simMongoDb(1, array=False)
     start = time()
-    writeText(cur[:10000])
+    writeText(data)
     print(time()-start)
